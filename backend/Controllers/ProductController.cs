9beccc6e3cf54a4cfdec5353867b255376420eba
Thanh-Phat <nguyenphat.300305@gmail.com>
2026-05-04 21:18:11 +0700

    feat(product): add product service, controller, DTO with filtering and search

---
+++ backend/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QRRestaurant_backend.Entities;
+using QRRestaurant_backend.Services.Interfaces;
+
+namespace QRRestaurant_backend.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+        //Get: api/products
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _service.GetAllAsync();
+            return Ok(data);
+        }
+
+
+        //Get: api/products/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await _service.GetByIdAsync(id);
+            if (data == null)
+                return NotFound();
+            return Ok(data);
+        }
+
+        //Get: api/products/category/{Id}
+        [HttpGet("category/{categoryId}")]
+        public async Task<IActionResult> GetByCategoryId(int categoryId)
+        {
+            var data = await _service.getByCategoryAsync(categoryId);
+            return Ok(data);
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search(string keyword)
+        {
+            var data = await _service.SearchAsync(keyword);
+                return Ok(data);
+        }
+    } 
+}
---
+++ backend/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using QRRestaurant_backend.DTOs.Product;
+
+namespace QRRestaurant_backend.Services.Interfaces
+{
+    public interface IProductService  
+    {
+        Task<List<ProductDto>> GetAllAsync();
+        Task<ProductDto?> GetByIdAsync(int id);
+        Task<List<ProductDto>> GetByCategoryAsync(int categoryId);
+        Task<List<ProductDto>> SearchAsync(string keyword);
+    }
+}
